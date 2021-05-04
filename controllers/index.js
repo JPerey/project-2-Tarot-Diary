49b29c9c7c7693a854e3b2971ee92e289ca1050a
James Perey <jamjam.perey@gmail.com>
2021-05-04 03:45:27 -0700

    COMPLETE: ver 1.0 done time to upload to heroku

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,16 @@
 module.exports = {
     index,
+    info,
 };
 
 function index(req, res) {
     res.render("index", {
         title: "Tarot Diary Home",
+    });
+};
+
+function info(req, res) {
+    res.render("info", {
+        title: "Tarot Reading Definitions",
     });
 };
--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const indexCtrl = require("../controllers/index");
 
 router.get("/", indexCtrl.index);
+router.get("/info", indexCtrl.info);
 
 module.exports = router;
